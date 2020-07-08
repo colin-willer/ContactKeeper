4149df5a2bb6e147d25dedb6bf39571d94533aff
colin <cowwiller@gmail.com>
2020-07-08 08:44:04 -0500

    Add PUT Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -53,8 +53,33 @@ router.post('/', [ auth, [ check('name', 'Name is required').not().isEmpty() ] ]
 // @route   PUT api/contacts/:id
 // @desc    Update contact info
 // @access  Private
-router.put('/:id', (req, res) => {
-	res.send('Update contact info');
+router.put('/:id', auth, async (req, res) => {
+	const { name, email, phone, type } = req.body;
+
+	// Build contact object
+	const contactFields = {};
+	if (name) contactFields.name = name;
+	if (email) contactFields.email = email;
+	if (phone) contactFields.phone = phone;
+	if (type) contactFields.type = type;
+
+	try {
+		let contact = await Contact.findById(req.params.id);
+
+		if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+		// Make sure user owns the contact
+		if (contact.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Not authorized' });
+		}
+
+		contact = await Contact.findByIdAndUpdate(req.params.id, { $set: contactFields }, { new: true });
+
+		res.json(contact);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   DELETE api/contacts/:id
