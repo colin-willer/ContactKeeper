daa21793bee1bfa1649af901311b8e69b914e4d1
colin <cowwiller@gmail.com>
2020-07-09 12:25:20 -0500

    Add user registration from UI to backend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
 	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
 
 	const { setAlert } = alertContext;
+	const { register, error, clearErrors } = authContext;
+
+	useEffect(
+		() => {
+			if (error === 'User already exists') {
+				setAlert(error, 'danger');
+				clearErrors();
+			}
+		},
+		[ error ]
+	);
 
 	const [ user, setUser ] = useState({
 		name: '',
@@ -27,7 +40,11 @@ const Register = () => {
 		} else if (password.length < 6) {
 			setAlert('Password must be greater than 6 characters', 'danger');
 		} else {
-			console.log('Register Submitted');
+			register({
+				name,
+				email,
+				password
+			});
 		}
 	};
 
